a6c08bf78f004b7d6629a824dd6f1eaf0f3afbed
reza saputra <rezasaputraa0707@gmail.com>
2025-05-08 16:48:18 +0700

    ongoing Landing pafge

--- template/header_user.php
+++ template/header_user.php
@@ -16,13 +16,13 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collaps" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="/user/dashboard.php">Dashboard</a></li>
                     <li class="nav-item"><a class="nav-link" href="/galeri.php">Galeri</a></li>
                     <li class="nav-item"><a class="nav-link" href="/booking.php">Pemesanan</a></li>
                     <li class="nav-item"><a class="nav-link" href="/contact.php">Kontak</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/wisata/deskripsi.php">Wisata</a></li>
+                    <li class="nav-item"><a class="nav-link" href="project_pweb2">Wisata</a></li>
                     <li class="nav-item"><a class="nav-link" href="/logout.php">Logout</a></li>
                 </ul>
             </div>
